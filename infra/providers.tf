24c15695a189dce438ac7d0a80121bb1e209ca73
Hugon Sroka <hugonsroka@gmail.com>
2025-07-21 11:36:00 +0200

    provider amendments

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.85.0"  # Sprawdź najnowszą wersję na https://registry.terraform.io/providers/hashicorp/google/latest
+    }
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = "europe-west3"
